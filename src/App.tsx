2f9ac7daa47e5f3e42c509d19985b820e393592f
sbadal007 <36693707+sbadal007@users.noreply.github.com>
2025-05-27 16:49:43 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import { useState } from "react";
-import "./App.css";
-import { EnvelopePage } from "./pages/EnvelopePage";
 import { InvitationPage } from "./pages/InvitationPage";
 
 type Language = "en" | "np";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
   const [language, setLanguage] = useState<Language>("en");
 
   const toggleLanguage = () => {
-    setLanguage((prev) => (prev === "en" ? "np": "en"));
+    setLanguage((prev) => (prev === "en" ? "np" : "en"));
   };
-  return (
-    <div className="App">
-      {isOpen ? (
-        <InvitationPage language={language} toggleLanguage={toggleLanguage} />
-      ) : (
-        <EnvelopePage onOpen={() => setIsOpen(true)} />
-      )}
-    </div>
-  );
+
+  return <InvitationPage language={language} toggleLanguage={toggleLanguage} />;
 }
 
 export default App;
