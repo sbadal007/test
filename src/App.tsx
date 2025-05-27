0b030a5d1a08227ae24c304ba93cf225ab4475a5
sbadal007 <36693707+sbadal007@users.noreply.github.com>
2025-05-27 16:47:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import "./App.css";
 import { EnvelopePage } from "./pages/EnvelopePage";
 import { InvitationPage } from "./pages/InvitationPage";
 
+type Language = "en" | "np";
+
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  const [language, setLanguage] = useState("en");
+  const [language, setLanguage] = useState<Language>("en");
 
   const toggleLanguage = () => {
-    setLanguage(prev => (prev === "en" ? "np" : "en"));
+    setLanguage((prev) => (prev === "en" ? "np" : "en"));
   };
-
   return (
     <div className="App">
       {isOpen ? (
