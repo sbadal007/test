4b525a60cdb14b27740ef3e1e1c4025f6d511a29
sbadal007 <36693707+sbadal007@users.noreply.github.com>
2025-05-27 16:14:02 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import { useState } from "react";
-import "./App.css"; // for envelope styling
+import { EnvelopePage } from "./pages/EnvelopePage";
+import { InvitationPage } from "./pages/InvitationPage";
 
 function App() {
   const [opened, setOpened] = useState(false);
 
   return (
-    <div className="app-container">
-      {!opened ? (
-        <div className="envelope-wrapper" onClick={() => setOpened(true)}>
-          <div className="envelope">
-            <div className="flap"></div>
-            <div className="body"></div>
-            <div className="letter">Tap to open</div>
-          </div>
-        </div>
-      ) : (
-        <div className="invitation">
-          <h1>You're Invited!</h1>
-          <p>Join us in celebrating our special day 💍</p>
-          {/* Add photo, countdown, RSVP, etc. */}
-        </div>
-      )}
-    </div>
+    <>
+      {opened ? <InvitationPage /> : <EnvelopePage onOpen={() => setOpened(true)} />}
+    </>
   );
 }
 
